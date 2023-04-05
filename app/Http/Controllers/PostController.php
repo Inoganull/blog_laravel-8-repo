a8114d679794a623ba3dcb7f82a047b752b3a6d8
Kyaw Min Hein <kyawminhein101@gmail.com>
2023-04-05 14:35:12 +0630

    authorized

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
     public function index()
     {
+        Gate::allows('admin');
         return view('posts.index', [
         'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(4)->withQueryString(), 
         ]);
--- routes/web.php
+++ routes/web.php
@@ -41,12 +41,18 @@ Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
-Route::get('admin/posts', [AdminPostController::class, 'index'])->middleware('admin');
-Route::post('admin/posts', [AdminPostController::class, 'store'])->middleware('admin');
-Route::get('admin/posts/create', [AdminPostController::class, 'create'])->middleware('admin');
-Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('admin');
-Route::patch('admin/posts/{post}', [AdminPostController::class, 'update'])->middleware('admin');
-Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy'])->middleware('admin');
+//Admin Section
+Route::middleware('can:admin')->group(function () {
+    Route::resource('admin/posts', AdminPostController::class)->except('show');
+});
+
+
+// Route::get('admin/posts', [AdminPostController::class, 'index'])->middleware('admin');
+// Route::post('admin/posts', [AdminPostController::class, 'store'])->middleware('admin');
+// Route::get('admin/posts/create', [AdminPostController::class, 'create'])->middleware('admin');
+// Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('admin');
+// Route::patch('admin/posts/{post}', [AdminPostController::class, 'update'])->middleware('admin');
+// Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy'])->middleware('admin');
 
 
 
